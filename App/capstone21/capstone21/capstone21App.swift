78eb207371ba0ae667e9ad6107353906be51dc60
HELLOHIDI <fbgmlwo123@naver.com>
2025-03-13 14:00:32 +0900

    [Setting] Project 생성 #4

---
+++ App/capstone21/capstone21/capstone21App.swift
@@ -0,0 +1,17 @@
+//
+//  capstone21App.swift
+//  capstone21
+//
+//  Created by 류희재 on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct capstone21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
