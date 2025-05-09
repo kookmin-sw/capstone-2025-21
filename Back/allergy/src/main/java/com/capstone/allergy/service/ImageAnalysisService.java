d702f2400c6c9d84d0cede2aa2d64c8b42eb97de
nf1kelly <nf1kelly52@gmail.com>
2025-05-10 01:14:34 +0900

    [Chore] nationality edit #115

--- Back/allergy/src/main/java/com/capstone/allergy/service/ImageAnalysisService.java
+++ Back/allergy/src/main/java/com/capstone/allergy/service/ImageAnalysisService.java
@@ -40,8 +40,8 @@ public class ImageAnalysisService {
     private String mapNationalityToLangCode(String nationality) {
         switch (nationality.toUpperCase()) {
             case "USA": return "en";
-            case "JAPAN": return "jp";
-            case "CHINA": return "ch";
+            case "JPN": return "jp";
+            case "CHN": return "ch";
             default: return "ko"; // 기본값
         }
     }
