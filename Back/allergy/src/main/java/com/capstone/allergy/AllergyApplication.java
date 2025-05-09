4b2adf39ed3089648824b56c09f6f64e46ddca64
nf1kelly <nf1kelly52@gmail.com>
2025-05-10 02:09:47 +0900

    [Chore] UploadScheduler #121

--- Back/allergy/src/main/java/com/capstone/allergy/AllergyApplication.java
+++ Back/allergy/src/main/java/com/capstone/allergy/AllergyApplication.java
@@ -2,8 +2,10 @@ package com.capstone.allergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AllergyApplication {
 
 	public static void main(String[] args) {
